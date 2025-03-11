119cd6d3594dada9ff38f26cde18e218315efeb1
Ehsanul Mostofa <emrafi789@gmail.com>
2025-03-10 22:04:53 -0400

    new UI except validation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,12 +31,13 @@ class HomeController < ApplicationController
     begin
       session[:csv_data] = service.process_csv(params[:file])
       flash[:notice] = "CSV with #{session[:csv_data].size} rows uploaded successfully!"
+      redirect_to managefile_path
     rescue EmploymentContractService::InvalidCSVError => e
       flash[:alert] = e.message
       session[:csv_data] = nil
+      set_template_and_preview_data # Call the helper method
+      render :managefile # Render the same page instead of redirecting
     end
-    
-    redirect_to root_path
   end
 
   def download_pdf
@@ -53,6 +54,48 @@ class HomeController < ApplicationController
                 disposition: 'attachment'
     rescue StandardError => e
       redirect_to root_path, alert: "Error generating PDFs: #{e.message}"
+    end
+  end
+
+  def managefile
+    set_template_and_preview_data # Call the helper method
+    Rails.logger.info "Selected template: #{params[:template]}"
+    Rails.logger.info "Rendering partial: #{@template_partial}"
+  end
+
+  private
+
+  def set_template_and_preview_data
+    template = params[:template] || 'default' # Default to 'default' if no template is provided
+    @template_partial = case template
+                        when 'default'
+                          'home/template'
+                        when 'template_1'
+                          'home/template_1'
+                        when 'template_2'
+                          'home/template_2'
+                        when 'template_3'
+                          'home/template_3'
+                        else
+                          'home/template'
+                        end
+  
+    # Use session[:csv_data] or default data for the preview
+    if session[:csv_data].present?
+      @preview_data = session[:csv_data].first.transform_keys(&:to_sym)
+    else
+      @preview_data = {
+        business_name: "Default Business Name",
+        employer_name: "Default Employer Name",
+        employer_email: "default@example.com",
+        employer_contact: "000-000-0000",
+        employer_address: "Default Address",
+        employee_name: "Default Employee Name",
+        employee_email: "default@example.com",
+        employee_contact: "000-000-0000",
+        employee_address: "Default Address",
+        duration_months: "0"
+      }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+
 Rails.application.routes.draw do
   root "home#index"
   get "/about", to: "home#about"
   post "upload_csv", to: "home#upload_csv"
   get "download_pdf", to: "home#download_pdf"
   get 'template', to: 'home#template'
+   get 'managefile', to: 'home#managefile', as: 'managefile'
+    get 'template_1', to: 'home#template_1'
+    get 'template_2', to: 'home#template_2'
+    get 'template_3', to: 'home#template_3'
   resources :documents, only: [:create, :destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
